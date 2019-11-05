da38082c0e66d9d23693ed0e0a42a15347763132
Francois David <francois.da@hotmail.com>
2019-11-04 21:05:25 -0500

    Update MainGameLoop.cpp

--- A2COMP345/A2_Part_6/MainGameLoop.cpp
+++ A2COMP345/A2_Part_6/MainGameLoop.cpp
@@ -120,7 +120,7 @@ void MainGameLoop::traverse(Turn *last)
         // Select one of the six cards facing up
         int indexOfCard = rand() % + 5;
         
-        Card *card = handObject->getCard(indexOfCard);
+        Card *card = handObject->exchange(indexOfCard);
         // Player pays for card
         p->player.BuyCard(indexOfCard);
         cout << "Good: " << card->getGoods() << endl;
