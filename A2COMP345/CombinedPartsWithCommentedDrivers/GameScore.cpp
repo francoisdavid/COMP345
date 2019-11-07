ef96bf8a3167b983bf8ec8dcb0f7a38981b13087
glowstarraw <38768667+glowstarraw@users.noreply.github.com>
2019-11-07 03:11:21 -0500

    Finalized part 6

--- A2COMP345/CombinedPartsWithCommentedDrivers/GameScore.cpp
+++ A2COMP345/CombinedPartsWithCommentedDrivers/GameScore.cpp
@@ -27,134 +27,13 @@ GameScore::~GameScore() {
 
 // Compute game score for each player based on criteria
 void GameScore::computeGameScore() {
+    processRegionsScore();
+    processContinentsScore();
+    listScoreAndStatistics();
+    
+     // Determine winner
     if (isGameEnd()) {
-        processRegionsScore();
-        
-        vector<int> playerScores;
-        for (int p = 0; p < players.size(); p++) {
-            playerScores.push_back(0);
-        }
-        
-        // Compute total score
-        for (int i = 0; i < players.size(); i++) {
-            
-            int playerScore = 0;
-            
-            // Compute regions score
-            playerScore += players.at(i)->getCountries().size();
-            
-            // Compute goods score
-            playerScore += computeGoodsScore(players.at(i));
-            
-            cout << "Player " << to_string(i + 1) << ": " << playerScore << endl;
-            playerScores[i] = playerScore;
-        }
-        
-        // Determine winner
-        int max = 0;
-        
-        for (int i = 0; i < playerScores.size(); i++) {
-            if (playerScores.at(i) > max) {
-                max = playerScores.at(i);
-            }
-        }
-        
-        int counter = 0;
-        for (int k = 0; k < playerScores.size(); k++) {
-            if (playerScores.at(k) == max) {
-                counter++;
-            }
-        }
-        
-        // There is only one winner with max score
-        if (counter == 1) {
-            for (int k = 0; k < playerScores.size(); k++) {
-                if (playerScores.at(k) == max) {
-                    cout << "Winner is " << "player " << to_string(k+1) << ": " << players.at(k)->getName() << endl;
-                }
-            }
-        }
-        
-        // If tied, check armies
-        else {
-            int maxArmies = 0;
-            
-            for (int i = 0; i < players.size(); i++) {
-                if (players.at(i)->getArmyCount() > maxArmies) {
-                    maxArmies = players.at(i)->getArmyCount();
-                }
-            }
-            
-            int armiesCounter = 0;
-            for (int k = 0; k < players.size(); k++) {
-                if (players.at(k)-> getArmyCount() == maxArmies) {
-                    armiesCounter++;
-                }
-            }
-            
-            // There is only one winner with max armies
-            if (armiesCounter == 1) {
-                for (int k = 0; k < players.size(); k++) {
-                    if (players.at(k)->getArmyCount() == maxArmies) {
-                        cout << "Winner is " << "player " << to_string(k+1) << ": " << players.at(k)->getName() << endl;
-                    }
-                }
-            }
-            
-            // If tied, check coins
-            else {
-                int maxCoins = 0;
-                
-                for (int i = 0; i < players.size(); i++) {
-                    if (*players.at(i)->getPlayerCoins() > maxCoins) {
-                        maxCoins = *players.at(i)->getPlayerCoins();
-                    }
-                }
-                
-                int coinsCounter = 0;
-                for (int k = 0; k < players.size(); k++) {
-                    if (*players.at(k)-> getPlayerCoins() == maxCoins) {
-                        coinsCounter++;
-                    }
-                }
-                
-                // There is only one winner with max armies
-                if (coinsCounter == 1) {
-                    for (int k = 0; k < players.size(); k++) {
-                        if (*players.at(k)->getPlayerCoins() == maxCoins) {
-                            cout << "Winner is " << "player " << to_string(k+1) << ": " << players.at(k)->getName() << endl;
-                        }
-                    }
-                }
-                
-                // If tied, check regions
-                /*else {
-                    int maxCountries = 0;
-                    
-                    for (int i = 0; i < players.size(); i++) {
-                        if (players.at(i)->getCountries().size() > maxCoins) {
-                            maxCoins = *players.at(i)->getPlayerCoins();
-                        }
-                    }
-                    
-                    int coinsCounter = 0;
-                    for (int k = 0; k < players.size(); k++) {
-                        if (*players.at(k)-> getPlayerCoins() == maxCoins) {
-                            coinsCounter++;
-                        }
-                    }
-                    
-                    // There is only one winner with max armies
-                    if (coinsCounter == 1) {
-                        for (int k = 0; k < players.size(); k++) {
-                            if (*players.at(k)->getPlayerCoins() == maxCoins) {
-                                cout << "Winner is " << "player " << to_string(k+1) << ": " << players.at(k)->getName() << endl;
-                            }
-                        }
-                    }
-                }*/
-            }
-        }
+        determineWinnerByScore();
     }
 }
 
@@ -170,7 +49,7 @@ bool GameScore::isGameEnd() {
         }
         
         if (isGameEnd) {
-            cout << "STOP GAME. " << "All players are currently holding 13 cards." << endl;
+            cout << endl << "STOP GAME. " << "All players are currently holding 13 cards." << endl;
         }
     }
     else if (players.size() == 3) {
@@ -181,7 +60,7 @@ bool GameScore::isGameEnd() {
         }
         
         if (isGameEnd) {
-            cout << "STOP GAME. " << "All players are currently holding 10 cards." << endl;
+            cout << endl << "STOP GAME. " << "All players are currently holding 10 cards." << endl;
         }
     }
     else if (players.size() == 4) {
@@ -192,7 +71,7 @@ bool GameScore::isGameEnd() {
         }
         
         if (isGameEnd) {
-            cout << "STOP GAME. " << "All players are currently holding 8 cards." << endl;
+            cout << endl << "STOP GAME. " << "All players are currently holding 8 cards." << endl;
         }
         
     }
@@ -204,56 +83,128 @@ bool GameScore::isGameEnd() {
         }
         
         if (isGameEnd) {
-            cout << "STOP GAME. " << "All players are currently holding 7 cards." << endl;
+            cout << endl << "STOP GAME. " << "All players are currently holding 7 cards." << endl;
         }
     }
     
     return isGameEnd;
 }
 
+// Determine winner for each country/region node
 void GameScore::processRegionsScore() {
     vector<Node*> countries = map->getCountries();
-    vector<int> playerCityCounts;
-    
-    for (int p = 0; p < players.size(); p++) {
-        playerCityCounts.push_back(0);
-    }
     
     // Compute region score
     for (int i = 0; i < countries.size(); i++) {
         vector<Army*> armies = countries.at(i)->getArmies();
-        vector<City*> cities = countries.at(i)->getCities();
+        vector<int> playerArmyCityCounts;
         
-        // Process city counts
-        for (int c = 0; c < cities.size(); c++) {
-            int ownerNumber = *cities.at(c)->getOwnerNumber() - 1;
-            playerCityCounts[ownerNumber]++;
+        // Instantiate
+        for (int i = 0; i < players.size(); i++) {
+            playerArmyCityCounts.push_back(0);
         }
         
-        int max = 0;
+        // Army count based on # of soliders placed in node
+        for (int d = 0; d < map->getCountries().at(i)->getArmies().size(); d++) {
+            int numOfSoldiers = *map->getCountries().at(i)->getArmies()[d]->getNumberOfSoldiers();
+            players[*map->getCountries().at(i)->getArmies()[d]->getOwnerNumber() - 1]->setPlayerNumOfArmiesBasedOnSoldiers(numOfSoldiers);
+        }
         
+        // Find player with max combo of # of armies and # of cities in a given location
         for (int j = 0; j < armies.size(); j++) {
-            if (*armies.at(j)->getNumberOfSoldiers() > max) {
-                max = *armies.at(j)->getNumberOfSoldiers();
-            }
-        }
-        
-        // Treat countries with armies
-        if (max > 0) {
-            int counter = 0;
-            for (int k = 0; k < armies.size(); k++) {
-                if (*armies.at(k)->getNumberOfSoldiers() == max) {
-                    counter++;
+            int currentOwnerNumber = *armies.at(j)->getOwnerNumber() - 1;
+            int numOfCities = 0;
+            
+            // Process each city in this location to see to whom it belongs to
+            for (int h = 0; h < countries.at(i)->getCities().size(); h++) {
+                vector<City*> playerCities = players[currentOwnerNumber]->getCities();
+                City* city = countries[i]->getCities()[h]; // Current city in iteration
+                
+                if (*city->getOwnerNumber() == (currentOwnerNumber + 1)) {
+                    auto result = find(playerCities.begin(), playerCities.end(), city);
+                    if (result != playerCities.end()) {
+                        numOfCities++;
+                    }
                 }
             }
             
-            // There is only one winner per region/country
-            if (counter == 1) {
-                for (int k = 0; k < armies.size(); k++) {
-                    if (*armies.at(k)->getNumberOfSoldiers() == max) {
-                        int ownerNumber = *armies.at(k)->getOwnerNumber() - 1;
-                        players.at(ownerNumber)->addCountry(countries.at(i));
-                    }
+            // Store count for comparison later
+            int total = *armies.at(j)->getNumberOfSoldiers() + numOfCities;
+            playerArmyCityCounts[currentOwnerNumber] += total;
+        }
+        
+        // Get highest score
+        int max = 0;
+        for (int i = 0; i < playerArmyCityCounts.size(); i++) {
+            if (playerArmyCityCounts[i] > max) {
+                max = playerArmyCityCounts[i];
+            }
+        }
+        
+        // Determine country winner by highest score for country
+        int counter = 0;
+        for (int k = 0; k < playerArmyCityCounts.size(); k++) {
+            if (playerArmyCityCounts[k] == max) {
+                counter++;
+            }
+        }
+        
+        // There is only one winner per region/country
+        if (counter == 1) {
+            for (int k = 0; k < playerArmyCityCounts.size(); k++) {
+                if (playerArmyCityCounts[k] == max) {
+                    players.at(k)->addCountry(countries[i]);
+                }
+            }
+        }
+    }
+}
+
+// Determine winner for each continent
+void GameScore::processContinentsScore() {
+    vector<Map*> continents = map->getContinents();
+    
+    for (int i = 0; i < continents.size(); i++) {
+        vector<int> playerCountryCounts;
+        for (int c = 0; c < players.size(); c++) {
+            playerCountryCounts.push_back(0);
+        }
+        
+        // Process each country within given continent
+        vector<Node*> countries = continents[i]->getCountries();
+        for (int j = 0; j < countries.size(); j++) {
+            
+            for (int k = 0; k < players.size(); k++) {
+                vector<Node*> playerCountries = players[k]->getCountries();
+                
+                auto result = find(playerCountries.begin(), playerCountries.end(), countries[j]);
+                
+                if (result != playerCountries.end()) {
+                    playerCountryCounts[k]++;
+                }
+            }
+        }
+        
+        // Determine which player has highest count of countries
+        int max = 0;
+        for (int x = 0; x < playerCountryCounts.size(); x++) {
+            if (playerCountryCounts[x] > max) {
+                max = playerCountryCounts[x];
+            }
+        }
+        
+        int counter = 0;
+        for (int y = 0; y < playerCountryCounts.size(); y++) {
+            if (playerCountryCounts[y] == max) {
+                counter++;
+            }
+        }
+        
+        // There is only one winner with max score
+        if (counter == 1) {
+            for (int z = 0; z < playerCountryCounts.size(); z++) {
+                if (playerCountryCounts[z] == max) {
+                    players[z]->addContinent(continents[i]);
                 }
             }
         }
@@ -287,6 +238,7 @@ int GameScore::computeGoodsScore(Player* player) {
     return goodsScore;
 }
 
+// Compute score based on carrot goods
 int GameScore::carrotsScore(int carrots){
     int total = 0;
     
@@ -308,6 +260,7 @@ int GameScore::carrotsScore(int carrots){
     return total;
 }
 
+// Compute score based on ore goods
 int GameScore::oreScore(int ore){
     int total = 0;
     
@@ -329,6 +282,7 @@ int GameScore::oreScore(int ore){
     return total;
 }
 
+// Compute score based on crystal goods
 int GameScore::crystalScore(int crystal){
     int total = 0;
     
@@ -350,6 +304,7 @@ int GameScore::crystalScore(int crystal){
     return total;
 }
 
+// Compute score based on forest goods
 int GameScore::forestScore(int forest){
     int total = 0;
     
@@ -371,6 +326,7 @@ int GameScore::forestScore(int forest){
     return total;
 }
 
+// Compute score based on anvil goods
 int GameScore::anvilScore(int anvil){
     int total = 0;
     
@@ -390,4 +346,143 @@ int GameScore::anvilScore(int anvil){
         total = 5;
     
     return total;
+}
+
+// Display the score and stats for each player
+void GameScore::listScoreAndStatistics() {
+    cout << endl;
+    
+    for (int i = 0; i < players.size(); i++) {
+        int score = 0;
+        
+        // Compute regions score
+        score += players.at(i)->getCountries().size();
+        
+        // Compute continents score
+        score += players.at(i)->getContinents().size();
+        
+        // Compute goods score
+        score += computeGoodsScore(players.at(i));
+        
+        // Set and display score & statistics
+        players.at(i)->setPlayerScore(score);
+        
+        cout << "Player " << players.at(i)->getName() << " - score: " << players.at(i)->getPlayerScore() << ", coins: " << *players.at(i)->getPlayerCoins() << ", armies: " << players.at(i)->getArmyCountBasedOnSoldiers() << ", regions: " << players.at(i)->getCountries().size() << endl;
+    }
+}
+
+// Determine winner based on score
+void GameScore::determineWinnerByScore() {
+    int max = 0;
+    
+    for (int i = 0; i < players.size(); i++) {
+        if (players.at(i)->getPlayerScore() > max) {
+            max = players.at(i)->getPlayerScore();
+        }
+    }
+    
+    int counter = 0;
+    for (int k = 0; k < players.size(); k++) {
+        if (players.at(k)->getPlayerScore() == max) {
+            counter++;
+        }
+    }
+    
+    // There is only one winner with max score
+    if (counter == 1) {
+        for (int k = 0; k < players.size(); k++) {
+            if (players.at(k)->getPlayerScore() == max) {
+                cout << "Winner by score is " << "player " << players.at(k)->getName() << endl;
+            }
+        }
+    } else {
+        determineWinnerByCoins();
+    }
+}
+
+// Determine winner based on on # of armies
+void GameScore::determineWinnerByArmies() {
+    int max = 0;
+    
+    for (int i = 0; i < players.size(); i++) {
+        if (players[i]->getArmyCountBasedOnSoldiers() > max) {
+            max = players[i]->getArmyCountBasedOnSoldiers();
+        }
+    }
+    
+    int counter = 0;
+    for (int k = 0; k < players.size(); k++) {
+        if (players[k]->getArmyCountBasedOnSoldiers() == max) {
+            counter++;
+        }
+    }
+    
+    // There is only one winner with max armies
+    if (counter == 1) {
+        for (int k = 0; k < players.size(); k++) {
+            if (players[k]->getArmyCountBasedOnSoldiers() == max) {
+                cout << "Winner by armies is " << "player " << players.at(k)->getName() << endl;
+            }
+        }
+    } else {
+        determineWinnerByControlledRegions();
+    }
+}
+
+// Determine winner based on # of coins
+void GameScore::determineWinnerByCoins() {
+    int maxCoins = 0;
+    
+    for (int i = 0; i < players.size(); i++) {
+        if (*players.at(i)->getPlayerCoins() > maxCoins) {
+            maxCoins = *players.at(i)->getPlayerCoins();
+        }
+    }
+    
+    int coinsCounter = 0;
+    for (int k = 0; k < players.size(); k++) {
+        if (*players.at(k)-> getPlayerCoins() == maxCoins) {
+            coinsCounter++;
+        }
+    }
+    
+    // There is only one winner with max armies
+    if (coinsCounter == 1) {
+        for (int k = 0; k < players.size(); k++) {
+            if (*players.at(k)->getPlayerCoins() == maxCoins) {
+                cout << "Winner by coins is " << "player " << players.at(k)->getName() << endl;
+            }
+        }
+    } else {
+        determineWinnerByArmies();
+    }
+}
+
+// Determine winner based on highest # of controlled region
+void GameScore::determineWinnerByControlledRegions() {
+    int maxRegions = 0;
+    
+    for (int i = 0; i < players.size(); i++) {
+        if (players[i]->getCountries().size() > maxRegions) {
+            maxRegions = players[i]->getCountries().size();
+        }
+    }
+    
+    int counter = 0;
+    for (int k = 0; k < players.size(); k++) {
+        if (players[k]->getCountries().size() == maxRegions) {
+            counter++;
+        }
+    }
+    
+    // There is only one winner with max # of controlled regions
+    if (counter == 1) {
+        for (int k = 0; k < players.size(); k++) {
+            if (players.at(k)->getCountries().size() == maxRegions) {
+                cout << "Winner by controlled regions is " << "player " << players.at(k)->getName() << endl;
+            }
+        }
+    } else {
+        cout << "There is no winner." << endl;
+    }
 }
