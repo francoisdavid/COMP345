19e06120596254fe3e5bc740106e745ba4ba4a69
ramielkazma <43940538+ramielkazma@users.noreply.github.com>
2019-11-17 07:06:49 -0500

    Set up the observer class, need to complete update methods

---
+++ A3COMP345/CombinedWithMainDriver/cmake-build-debug/cmake-build-debug/CMakeFiles/CombinedWithMainDriver.dir/DependInfo.cmake
@@ -0,0 +1,32 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/francoisdavid/Desktop/COMP345/A3COMP345/CombinedWithMainDriver/Army.cpp" "/Users/francoisdavid/Desktop/COMP345/A3COMP345/CombinedWithMainDriver/cmake-build-debug/CMakeFiles/CombinedWithMainDriver.dir/Army.cpp.o"
+  "/Users/francoisdavid/Desktop/COMP345/A3COMP345/CombinedWithMainDriver/BidingFacility.cpp" "/Users/francoisdavid/Desktop/COMP345/A3COMP345/CombinedWithMainDriver/cmake-build-debug/CMakeFiles/CombinedWithMainDriver.dir/BidingFacility.cpp.o"
+  "/Users/francoisdavid/Desktop/COMP345/A3COMP345/CombinedWithMainDriver/Cards.cpp" "/Users/francoisdavid/Desktop/COMP345/A3COMP345/CombinedWithMainDriver/cmake-build-debug/CMakeFiles/CombinedWithMainDriver.dir/Cards.cpp.o"
+  "/Users/francoisdavid/Desktop/COMP345/A3COMP345/CombinedWithMainDriver/City.cpp" "/Users/francoisdavid/Desktop/COMP345/A3COMP345/CombinedWithMainDriver/cmake-build-debug/CMakeFiles/CombinedWithMainDriver.dir/City.cpp.o"
+  "/Users/francoisdavid/Desktop/COMP345/A3COMP345/CombinedWithMainDriver/GameObservers.cpp" "/Users/francoisdavid/Desktop/COMP345/A3COMP345/CombinedWithMainDriver/cmake-build-debug/CMakeFiles/CombinedWithMainDriver.dir/GameObservers.cpp.o"
+  "/Users/francoisdavid/Desktop/COMP345/A3COMP345/CombinedWithMainDriver/GameScore.cpp" "/Users/francoisdavid/Desktop/COMP345/A3COMP345/CombinedWithMainDriver/cmake-build-debug/CMakeFiles/CombinedWithMainDriver.dir/GameScore.cpp.o"
+  "/Users/francoisdavid/Desktop/COMP345/A3COMP345/CombinedWithMainDriver/MAINDriver.cpp" "/Users/francoisdavid/Desktop/COMP345/A3COMP345/CombinedWithMainDriver/cmake-build-debug/CMakeFiles/CombinedWithMainDriver.dir/MAINDriver.cpp.o"
+  "/Users/francoisdavid/Desktop/COMP345/A3COMP345/CombinedWithMainDriver/MainGameLoop.cpp" "/Users/francoisdavid/Desktop/COMP345/A3COMP345/CombinedWithMainDriver/cmake-build-debug/CMakeFiles/CombinedWithMainDriver.dir/MainGameLoop.cpp.o"
+  "/Users/francoisdavid/Desktop/COMP345/A3COMP345/CombinedWithMainDriver/Map.cpp" "/Users/francoisdavid/Desktop/COMP345/A3COMP345/CombinedWithMainDriver/cmake-build-debug/CMakeFiles/CombinedWithMainDriver.dir/Map.cpp.o"
+  "/Users/francoisdavid/Desktop/COMP345/A3COMP345/CombinedWithMainDriver/MapLoader.cpp" "/Users/francoisdavid/Desktop/COMP345/A3COMP345/CombinedWithMainDriver/cmake-build-debug/CMakeFiles/CombinedWithMainDriver.dir/MapLoader.cpp.o"
+  "/Users/francoisdavid/Desktop/COMP345/A3COMP345/CombinedWithMainDriver/Player.cpp" "/Users/francoisdavid/Desktop/COMP345/A3COMP345/CombinedWithMainDriver/cmake-build-debug/CMakeFiles/CombinedWithMainDriver.dir/Player.cpp.o"
+  "/Users/francoisdavid/Desktop/COMP345/A3COMP345/CombinedWithMainDriver/PlayerActions.cpp" "/Users/francoisdavid/Desktop/COMP345/A3COMP345/CombinedWithMainDriver/cmake-build-debug/CMakeFiles/CombinedWithMainDriver.dir/PlayerActions.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "AppleClang")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../."
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
